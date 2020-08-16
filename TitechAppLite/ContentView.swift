2aa7f1b79ab3c42c5a3b410d53e23da4d295598d
Hosshii <sao_heath6147.wistre@icloud.com>
2020-08-16 16:18:34 +0900

    toriaezu

--- TitechAppLite/ContentView.swift
+++ TitechAppLite/ContentView.swift
@@ -10,7 +10,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        List(0..<10){_ in
+            HStack {
+                Rectangle().fill(Color("main")).frame(width: 5, height: 100, alignment: .leading)
+            
+                VStack{
+                    Spacer()
+                    HStack{
+                        Text("today")
+                    }
+                    Spacer()
+                    HStack {
+                        Text("Today")
+                    }
+                    Spacer()
+                }
+                VStack(alignment: .leading){
+                    Spacer()
+                    HStack{
+                        Text("programming").lineLimit(2)
+                    }
+                    Spacer()
+                    HStack{
+                        Text("Week 1 Description")
+                    }
+                    Spacer()
+                }
+                Spacer()
+                Text("w222")
+                
+            }.frame(height: 100)
+        }
     }
 }
 
